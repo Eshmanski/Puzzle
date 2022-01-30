20d4fe367c704feb5a16eafcc0a798de8f2a73b7
Pavel <Eshmanski@gmail.com>
2022-01-30 16:25:08 +0300

    add docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'script.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   mode: "development",
   target: 'web',
